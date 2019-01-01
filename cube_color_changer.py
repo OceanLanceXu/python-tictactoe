99025d8d5ac0022a9eb76334422b8f346c9d3327
Ocean L Xu <ocean.lance.xu@gmail.com>
2019-01-01 18:47:56 -0500

    turn 1 board into 4 boards. used to be 9 squares to change colors, now with 36 squares to change colors. add arrows to cycle through the 4 boards. add counter on the clear button to keep track of the current board we are on.

--- cube_color_changer.py
+++ cube_color_changer.py
@@ -16,18 +16,21 @@ class Board:
     self.empty = -1
     self.size = 3
     self.fields = {}
-    for y in range(self.size):
-      for x in range(self.size):
-        self.fields[x,y] = self.empty
+    self.clear()
+
     # copy constructor
     if other:
       self.__dict__ = deepcopy(other.__dict__)
-      
+
   def cycle_color(self,x,y):
     index = self.fields[x,y]
     index = (index + 1) % len(Board.COLOR)
     self.fields[x,y] = index
-  
+
+  def clear(self):
+    for y in range(self.size):
+      for x in range(self.size):
+        self.fields[x,y] = self.empty
 
 class GUI:
 
@@ -35,28 +38,59 @@ class GUI:
     self.app = Tk()
     self.app.title('Cube Color Changer')
     self.app.resizable(width=False, height=False)
-    self.board = Board()
-    self.font = Font(family="Helvetica", size=32)
+    self.boards = []
+    for c in range(4):
+      self.boards.append(Board())
+    self.board_index = 0
+    self.board = self.boards[self.board_index]
+    self.font = Font(family="Helvetica", size=64)
     self.buttons = {}
     for x,y in self.board.fields:
       handler = lambda x=x,y=y: self.cycle_color(x,y)
       button = Button(self.app, command=handler, font=self.font, width=3, height=1)
       button.grid(row=y, column=x)
       self.buttons[x,y] = button
+
     handler = lambda: self.clear()
-    button = Button(self.app, text='clear', command=handler)
-    button.grid(row=self.board.size+1, column=0, columnspan=self.board.size, sticky="WE")
+    self.clear_button = Button(self.app, text='clear 0', command=handler)
+    self.clear_button.grid(row=self.board.size+1, column=1, columnspan=1, sticky="WE")
+
+    handler = lambda: self.change_board('left')
+    button = Button(self.app, text='<==', command=handler)
+    button.grid(row=self.board.size+1, column=0, columnspan=1, sticky="WE")
+
+    handler = lambda: self.change_board('right')
+    button = Button(self.app, text='==>', command=handler)
+    button.grid(row=self.board.size+1, column=2, columnspan=1, sticky="WE")
+
+
     self.update()
-    
+
+
+  def change_board(self, direction):
+    if direction == 'right':
+      self.board_index += 1
+      self.board_index = self.board_index % 4
+
+    elif direction == 'left':
+      self.board_index -= 1
+      self.board_index = self.board_index % 4
+    self.board = self.boards[self.board_index]
+    self.clear_button['text'] = 'clear ' + str(self.board_index)
+
+    print('change_board:', self.board_index)
+    self.update()
+    #self.app.update()
+
   def clear(self):
-    self.board = Board()
+    self.board.clear()
     self.update()
-  
+
   def cycle_color(self,x,y):
-    print(x, y)
     self.app.update()
     self.board.cycle_color(x,y)
     self.update_cell(x, y)
+    print('cycle_color:', self.board_index, x, y, self.buttons[x,y]['bg'])
 
   def update_cell(self, x, y):
     color_index = self.board.fields[x,y]
